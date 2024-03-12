4f5be14e5a04139017e4423df805a70612bd70f1
Christy Pessagno <cpessagno12@gmail.com>
2024-03-12 19:20:32 +0000

    Completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,23 @@
+// Define new variable named languages
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+
+// Log the languages variable to the console
+console.log(languages);
+
+// Declare a new variable named firstElement
+let firstElement = languages[0];
+
+// Log the value of your firstElement variable
+console.log('The first element in the array is ' + firstElement);
+
+// Declare a variable named thirdElement
+let thirdElement = languages[2];
+console.log('The third element of the array is ' + thirdElement);
+
+// Declare a variable named length
+let length = languages.length;
+console.log('The length of the array is ' + length);
+
+// Declare a variable named lastElement
+let lastElement = languages[languages.length - 1];
+console.log('The last element of the array is ' + lastElement);
